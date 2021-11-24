364fc989a63faf3965bb0da983dbc4cacc2605dc
Yeison <enmanuelquiterios@gmail.com>
2021-11-23 23:35:58 -0500

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(user_params)
+        render json: user, include: :items
+    end
+
+
+    private 
+
+    def user_params 
+        user = params.permit(:id)
+    end
+
+
 end
